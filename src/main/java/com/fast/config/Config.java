dd8fee7c1cfea9a1c1ffaab33c80f2d80d92d09a
박준현 <gktkwk91@naver.com>
2024-05-09 19:15:06 +0900

    Implement #1 - webMVC
    웹 MVC 적용 및 Main은 불필요하기때문에 삭제

--- src/main/java/com/fast/config/Config.java
+++ src/main/java/com/fast/config/Config.java
@@ -2,7 +2,9 @@ package com.fast.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.fast")
 @Configuration
 public class Config {
