ba0ff5ae7432fc79387f9b6ee2845e5a5276f24b
박준현 <gktkwk91@naver.com>
2024-05-09 16:15:34 +0900

    apply #1 -  spring configuration
    기초 설정
    루트 패키지 이하에 빈이 있는지 스캔한다.

---
+++ src/main/java/com/fast/config/Config.java
@@ -0,0 +1,9 @@
+package com.fast.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.fast")
+@Configuration
+public class Config {
+}
